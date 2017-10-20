26c345dc7dee56fce077efb2a9ad13b640c9e8a1
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-20 10:25:53 -0700

    create getDoctor prototype that will display doctors names from api request to UI

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,1 +1,9 @@
 import { DoctorLookup } from './../js/doctor-lookup.js';
+
+$(document).ready(function(){
+  const doctorsResult = new DoctorLookup();
+
+  $(".btn").click(function(){
+    doctorsResult.getDoctors();
+  })
+});
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,4 +1,16 @@
 export class DoctorLookup {
   constructor(){
   }
+
+  getDoctors(){
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=47.608013, -122.335167,100&skip=0&limit=3&user_key=3df2d26e3fe30c15f597ce18fedf4144`)
+    .then(function(response){
+      const doctors = response.data;
+      doctors.forEach(function(doctor){
+        $("#doctor-results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`)
+      });
+    }).fail(function(error){
+      $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
+  }
 }
