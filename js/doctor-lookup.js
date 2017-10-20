e53c4bb5e5b114c88e39d7ebf02020e5ca6db8d8
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-20 11:31:53 -0700

    add doctors image to UI

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -3,15 +3,17 @@ export class DoctorLookup {
   }
 
   getDoctors(){
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=47.608013, -122.335167,100&skip=0&limit=3&user_key=3df2d26e3fe30c15f597ce18fedf4144`)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=47.608013, -122.335167,100&skip=0&limit=100&user_key=3df2d26e3fe30c15f597ce18fedf4144`)
     .then(function(response){
       const doctors = response.data;
       doctors.forEach(function(doctor){
         $("#doctor-results").append(
           `<li class="well">
+            <img src=${doctor.profile.image_url} alt="Doctor's photo">
             <p>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name},${doctor.profile.title}</p>
             <p>${doctor.practices[0].visit_address.street}</p> <p>${doctor.practices[0].visit_address.city},${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}</p>
-            <p>${doctor.practices[0].phones[0].number}</p>
+            <p>${DoctorLookup.phoneNumberConverter(doctor.practices[0].phones[0].number)}</p>
+
           </li>`)
       });
     }).fail(function(error){
@@ -19,11 +21,11 @@ export class DoctorLookup {
     });
   }
 
-  booleanConverter(booleanResponse){
+  static booleanConverter(booleanResponse){
     return booleanResponse ? "Yes" : "No";
   }
 
-  phoneNumberConverter(phoneNumber){
+  static phoneNumberConverter(phoneNumber){
     let convertedNumber;
     let numbers = phoneNumber.split("");
     numbers.splice(3,0,"-");
