c3bb768bf371d0676d63dd371cf31b2703e0093c
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-20 12:11:44 -0700

    add if doctor is taken in patients and link to their websites

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -8,11 +8,15 @@ export class DoctorLookup {
       const doctors = response.data;
       doctors.forEach(function(doctor){
         $("#doctor-results").append(
-          `<li class="well col-sm-6 col-md-4">
+          `<li class="well col-sm-4 col-md-4">
             <img src=${doctor.profile.image_url} alt="Doctor's photo">
               <p>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name},${doctor.profile.title}</p>
               <p>${doctor.practices[0].visit_address.street}</p> <p>${doctor.practices[0].visit_address.city},${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}</p>
               <p>${DoctorLookup.phoneNumberConverter(doctor.practices[0].phones[0].number)}</p>
+              <div class="web-container">
+                <p>Accepting New Patients: ${DoctorLookup.booleanConverter(doctor.practices[0].accepts_new_patients)}</p>
+                <a href=${doctor.practices[0].website}><button type="button" class="btn btn-primary">Visit Doctor's Website</button></a>
+              </div>
           </li>`)
       });
     }).fail(function(error){
