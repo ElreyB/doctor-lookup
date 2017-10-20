b5ff7b91ce8ae08df43b4aa96ec04cf6bfc0ef4d
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-20 11:50:11 -0700

    add styles to doctors thumbnails

--- spec/doctor-lookup-spec.js
+++ spec/doctor-lookup-spec.js
@@ -8,14 +8,14 @@ describe('DoctorLookup', function(){
   });
 
   it('should return "Yes" when give true as an argument', function(){
-    expect(doctorsResult.booleanConverter(true)).toEqual("Yes")
+    expect(DoctorLookup.booleanConverter(true)).toEqual("Yes")
   });
 
   it('should return "No" when give false as an argument', function(){
-    expect(doctorsResult.booleanConverter(false)).toEqual("No")
+    expect(DoctorLookup.booleanConverter(false)).toEqual("No")
   });
 
   it('should return phone number in user friendly form', function(){
-    expect(doctorsResult.phoneNumberConverter("1111111111")).toEqual("111-111-1111")
+    expect(DoctorLookup.phoneNumberConverter("1111111111")).toEqual("111-111-1111")
   });
 });
