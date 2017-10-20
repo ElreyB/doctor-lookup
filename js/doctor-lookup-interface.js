0a752b3ffdca0f139976d000c3ab65654ee276b9
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-20 13:42:54 -0700

    show doctors by user issue input

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -3,7 +3,13 @@ import { DoctorLookup } from './../js/doctor-lookup.js';
 $(document).ready(function(){
   const doctorsResult = new DoctorLookup();
 
-  $(".btn").click(function(){
+  $(".all").click(function(){
     doctorsResult.getDoctors();
   })
+
+  $("form.issue-form").submit(function(e){
+    e.preventDefault();
+    const userIsssue = $("input#issue").val();
+    doctorsResult.getByIssue(userIsssue);
+  });
 });
