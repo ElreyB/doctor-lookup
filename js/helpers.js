c1f82f4e05ce2cddc48fc93b477c550b6dd0dce2
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-26 19:06:32 -0700

    redesign doctors information well

--- js/helpers.js
+++ js/helpers.js
@@ -1,21 +1,28 @@
 export function lineCreator(doctors) {
   doctors.forEach(function(doctor) {
     $("#doctor-results").append(
-      `<li class="well col-sm-12">
-        <img class="doctor-image" src=${checkImage(doctor.profile.image_url)} alt="Doctor's photo">
-        <a href=${doctor.practices[0].website}>
-        <h4>Dr. ${doctor.profile.first_name} ${doctor.profile
-        .last_name},${doctor.profile.title}</h4>
-        </a>
-				<p>${doctor.practices[0].visit_address.street}</p> <p>${doctor.practices[0]
-        .visit_address.city},${doctor.practices[0].visit_address.state} ${doctor
-        .practices[0].visit_address.zip}</p>
-				<p>${phoneNumberConverter(doctor.practices[0].phones[0].number)}</p>
-				<div class="web-container">
-				<p>Accepting New Patients: ${booleanConverter(
-          doctor.practices[0].accepts_new_patients
-        )}</p>
-				</div>
+      `<li class="well col-sm-8">
+      <div class="row">
+        <div class="col-md-6 doctor-info">
+          <img class="doctor-image" src=${checkImage(doctor.profile.image_url)} alt="Doctor's photo">
+          <a href=${doctor.practices[0].website}>
+          <h4 class="doctor-name">Dr. ${doctor.profile.first_name} ${doctor.profile
+            .last_name},${doctor.profile.title}</h4>
+            </a>
+        </div>
+        <div class="col-md-6 doctor-info">
+            <p>${doctor.practices[0].name}</p>
+            <p>${doctor.practices[0].visit_address.street}</p> <p>${doctor.practices[0]
+              .visit_address.city},${doctor.practices[0].visit_address.state} ${doctor
+                .practices[0].visit_address.zip}</p>
+                <p>${phoneNumberConverter(doctor.practices[0].phones[0].number)}</p>
+                <div class="web-container">
+                <p>Accepting New Patients: ${booleanConverter(
+                  doctor.practices[0].accepts_new_patients
+                )}</p>
+                </div>
+        </div>
+      </div>
       </li>`
     );
   });
@@ -51,7 +58,7 @@ function phoneNumberConverter(phoneNumber) {
   return convertedNumber;
 }
 
-function websiteFitter(website) {
-  let 
-  return website === 'undefined' ?
-}
+// function websiteFitter(website) {
+//   let
+//   return website === 'undefined' ?
+// }
