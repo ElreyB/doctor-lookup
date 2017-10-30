a3ae580dfaad7783272ce990a272d225aa05c233
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-29 17:54:58 -0700

    WIP

--- js/helpers.js
+++ js/helpers.js
@@ -3,14 +3,14 @@ export function lineCreator(doctors) {
     $("#doctor-results").append(
       `<li class="well col-sm-8">
       <div class="row">
-        <div class="col-md-6 doctor-info">
+        <div class="col-md-5 doctor-info">
           <img class="doctor-image" src=${checkImage(doctor.profile.image_url)} alt="Doctor's photo">
           <a href="${websiteFitter(doctor.practices[0].website)}">
           <h4 class="doctor-name">Dr. ${doctor.profile.first_name} ${doctor.profile
             .last_name},${doctor.profile.title}</h4>
             </a>
         </div>
-        <div class="col-md-6 doctor-info">
+        <div class="col-md-7 doctor-info">
             <p>${doctor.practices[0].name}</p>
             <p>${doctor.practices[0].visit_address.street}</p> <p>${doctor.practices[0]
               .visit_address.city},${doctor.practices[0].visit_address.state} ${doctor
